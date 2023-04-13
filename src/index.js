248da73c892a52c0d1788005d0d9056915aa476f
Otto <justigo86@gmail.com>
2023-04-13 19:08:59 -0400

    small update to cleanup index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import './Hello.css';
 import App from './containers/App';
-// import Hello from './Hello'
-// import Card from './Card';
-// import CardList from './CardList'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-// import {robots} from './robots';
 
-ReactDOM.render(        //we want the ReactDOM package to use render function to render code in render()
-  // <React.StrictMode>
-  //   <h1>Hello World</h1>
-  // </React.StrictMode>,
-  <div>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </div>
+  </React.StrictMode>
   , document.getElementById('root')
 );
 reportWebVitals();
